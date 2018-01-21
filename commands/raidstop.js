527c52ebed37edc6573ccb31301daa46f87327ad
Lux <khluxordx@gmail.com>
2018-01-21 22:16:34 +0000

    Added cancel argument and implemented stat track

--- commands/raidstop.js
+++ commands/raidstop.js
@@ -2,7 +2,11 @@
 module.exports.run = async (bot, message, args, cfg) => { //this is what will run when the command is called.
   if (cfg.activeRaid) {
       cfg.activeRaid = false;
-      message.channel.send('The raid is over! Thanks everyone!').catch(console.error);
+      if (args[0] != "cancel") {
+        message.channel.send('The raid is over! Thanks everyone!').catch(console.error);
+      } else {
+        message.channel.send("Something went wrong and the raid ended!").catch(console.error);
+      }
       clearInterval(cfg.timeInt);
       cfg.timer = 0;
 
@@ -16,6 +20,32 @@ module.exports.run = async (bot, message, args, cfg) => { //this is what will ru
           let member = message.guild.members.find(item => item.user.username === cfg.raidQueue[i]);
           member.removeRole(role).catch(console.error);
       }
+    
+    
+    //Track Stats
+    if (args[0] != "cancel") {
+      for (var i = 0; i < cfg.raidQueue.length; i++) {
+        //Sort through each player remaining in the raid queue and add to their raid count
+        
+        //Structure for raidStat: cfg.raidStat = [[user, raidCount]]
+        
+        //locally store user's current raid count item
+        for (var j = 0; j < cfg.raidStat.length; j++) { //Sort through each player's stats for a matching name
+          if (cfg.raidQueue[i] == cfg.raidStat[j][0]) { //If we have a name match...
+            cfg.hasStat = true;
+            cfg.raidStat[j][1]++; //...Increment their stat count
+          }
+        }
+        
+        if (!cfg.hasStat) { //This person does not have established stats yet; create them!
+          cfg.raidStat.push([cfg.raidQueue[i], 1]);
+        }
+        
+        cfg.hasStat = false; //Reset this value for the next person
+      }
+    }
+    
+    
       cfg.raidQueue = [];
       cfg.boosters = [];
       cfg.playerAttacking = 0;
