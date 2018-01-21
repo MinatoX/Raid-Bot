75a0f796f8151ec53fe8b62740e35ed67b0fc4df
JaredH08 <jmh2dj@mtmail.mtsu.edu>
2018-01-21 12:33:24 -0600

    reimplemented q-ing by giving user names

--- commands/q.js
+++ commands/q.js
@@ -1,6 +1,8 @@
 //ARG [PLAYERNAME] //Add a player to the queue
 module.exports.run = async (bot, message, args,cfg) => { //this is what will run when the command is called.
   //command logs that a user has boosted and adds them to the raid party
+  var response = "";
+  console.log(args[0]);
     if (args[0] === "rem"){//un do accidental boost call
 
         var i = cfg.raidQueue.indexOf(message.member.user.username);
@@ -14,8 +16,7 @@ module.exports.run = async (bot, message, args,cfg) => { //this is what will run
         member.removeRole(role).catch(console.error);
         message.channel.send("You have left from the Raid Party").catch(console.error);
 
-    }else{
-
+    }else if(args[0] == null){//queues the user that sent the command
           for (var i=0; i<cfg.raidQueue.length + 1; i++) {
 
               if (message.member.user.username == cfg.raidQueue[i - 1]) { //Make sure the player isn't already in the queue
@@ -36,12 +37,29 @@ module.exports.run = async (bot, message, args,cfg) => { //this is what will run
                 member.addRole(role).catch(console.error);//add raid party role to player
                 message.channel.send(`${message.member.user.username} has joined the raiding party!`).catch(console.error);
 
-              }else{//wrong argument was passed
-
-                message.channel.send(`${message.member.user.username}, ${args[0]} is not a correct argument. If your not rading please use ~b nr.`).catch(console.error);
-
               }
           }
+    }else{//queues players names that were given
+      let role = message.guild.roles.find("name", "Raid party");
+      let member = message.guild.members.find(item => item.user.username === args[0]);
+      if(member !== null){
+        member.addRole(role).catch(console.error);
+        for(var i = 0; i<args.length; i++){
+          console.log('in loop');
+          member = message.guild.members.find(item => item.user.username === args[i]);
+          member.addRole(role).catch(console.error);
+        }
+        for(var j = 0; j<args.length; j++){
+          response += `${args[j]} `;
+        }
+        message.channel.send(`${response} have been added.`).catch(console.error);
+      }else{//invalid arguments
+        for(var i = 0; i<args.length; i++){
+          response += `${args[i]} `;
+        }
+        message.channel.send(`argument not valid: ${response}`).catch(console.error);
+
+      }
     }
 }
 
