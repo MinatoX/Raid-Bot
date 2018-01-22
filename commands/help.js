b54716e91c32ed8dee03ca3f6ff5f961d1795269
JaredH08 <jmh2dj@mtmail.mtsu.edu>
2018-01-22 13:51:15 -0600

    Updated help list

--- commands/help.js
+++ commands/help.js
@@ -2,8 +2,11 @@
 module.exports.run = (client,message,args, cfg) => {
    message.channel.send({embed:{
       description:`\nAvailable commands:
-      \n~q [PLAYER(S)]: Adds player(s) to the raid queue
+      \n~q: Adds player to the raid queue, add multiple players by giving list of names
+      \n~b: Tells Bot you have boosted and adds you to raid queued
+      \n~b nr: Tells Bot you have boosted, does not add you to queue
       \n~qcheck: Lists all players queued
+      \n~chkboost: Lists all players who have boosted
       \n~qclear: Removes all players from raid queue
       \n~qremove [PLAYER]: Removes player from the raid queue
       \n~raidstart [TEMPLARS/PALADINS]: Starts the raid callouts for the respective group
@@ -15,7 +18,6 @@ module.exports.run = (client,message,args, cfg) => {
       \n~resume: Start the timer again
       \n~timer: Check the current time in seconds
       \n~reset: Sets timer to 0
-      \n~b: Tells Bot you have boosted
       \n~chkboost: See which members have boosted`,
       color: 0x17A589
     }});
