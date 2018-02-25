e6522125a14a620bb48bb3d253703c02c1abbf4e
jaja1113 <jaredh08@hotmail.com>
2018-02-24 18:08:16 -0600

    removed redundencies, dequeues by nickname, username if not one

--- commands/raidstop.js
+++ commands/raidstop.js
@@ -12,23 +12,20 @@ module.exports.run = async (bot, message, args, cfg) => { //this is what will ru
 
       //removes players in raid party role
       let role = message.guild.roles.find("name", "Raid party");
-      for(var i = 0; i < cfg.boosters.length; i++){
-          let member = message.guild.members.find(item => item.user.username === cfg.boosters[i]);
-          member.removeRole(role).catch(console.error);
-      }
+
       for(var i = 0; i < cfg.raidQueue.length; i++){
-          let member = message.guild.members.find(item => item.user.username === cfg.raidQueue[i]);
+          let member = message.guild.members.find(item => isUserName(item, cfg.raidQueue[i]));
           member.removeRole(role).catch(console.error);
       }
-    
-    
+
+
     //Track Stats
     if (args[0] != "cancel") {
       for (var i = 0; i < cfg.raidQueue.length; i++) {
         //Sort through each player remaining in the raid queue and add to their raid count
-        
+
         //Structure for raidStat: cfg.raidStat = [[user, raidCount]]
-        
+
         //locally store user's current raid count item
         for (var j = 0; j < cfg.raidStat.length; j++) { //Sort through each player's stats for a matching name
           if (cfg.raidQueue[i] == cfg.raidStat[j][0]) { //If we have a name match...
@@ -36,16 +33,16 @@ module.exports.run = async (bot, message, args, cfg) => { //this is what will ru
             cfg.raidStat[j][1]++; //...Increment their stat count
           }
         }
-        
+
         if (!cfg.hasStat) { //This person does not have established stats yet; create them!
           cfg.raidStat.push([cfg.raidQueue[i], 1]);
         }
-        
+
         cfg.hasStat = false; //Reset this value for the next person
       }
     }
-    
-    
+
+
       cfg.raidQueue = [];
       cfg.boosters = [];
       cfg.playerAttacking = 0;
@@ -55,6 +52,20 @@ module.exports.run = async (bot, message, args, cfg) => { //this is what will ru
 
   }
 
+}
+
+function isUserName(item, args){//checks for nickname, if none uses username
+  if(item.nickname !==null){
+      if(args.toLowerCase() === item.nickname.toLowerCase() || args.toLowerCase() === item.user.username.toLowerCase()){
+          return true;
+      }else{
+          return false;
+      }
+  }else if (item.user.username.toLowerCase() === args.toLowerCase()) {
+      return true;
+  } else {
+      return false;
+  }
 }
 
 module.exports.config = { //this is the config for the command, you can add things to this such as proper usage, ect.(jaja)
